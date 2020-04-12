ec26e3fcf2e7a159ffb092e55007af327024cf6a
Tyoukai <zhang_k_w@163.com>
2020-04-12 17:10:13 +0800

    init

---
+++ src/main/java/com/zkw/rabbitRPCConsumer/RabbitRpcConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.zkw.rabbitRPCConsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitRpcConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RabbitRpcConsumerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zkw/rabbitRPCConsumer/RabbitRpcConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zkw.rabbitRPCConsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitRpcConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
